741e5431f06a1b6779fe97bce4787a7cf854654a
Husky <Husky@shengjie.com>
2023-04-22 17:00:22 +0800

    修改内容模块

--- xuecheng-plus-parent/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/husky/content/api/CourseTeacherController.java
+++ xuecheng-plus-parent/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/husky/content/api/CourseTeacherController.java
@@ -9,9 +9,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +33,11 @@ public class CourseTeacherController {
     @ApiImplicitParam(value = "courseId",name = "课程Id",required = true,dataType = "Long",paramType = "path")
     public List<CourseTeacher> getTeacher(@PathVariable Long courseId){
         return courseTeacherService.getCourseTeacher(courseId);
+    }
+
+    @PostMapping("/courseTeacher")
+    @ApiOperation("添加教师接口")
+    public CourseTeacher addTeacher(@RequestBody AddCourseTeacherDto addCourseTeacherDto){
+        return null;
     }
 }
